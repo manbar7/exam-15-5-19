666645d603c5fead6cd3d80df1df3d3e6435f511
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-05-15 19:48:09 +0300

    Add files via upload

---
+++ ConsoleApp24/ItemAlreadyExistException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SecretAgent
+{
+    [Serializable]
+    internal class ItemAlreadyExistException : Exception
+    {
+        public ItemAlreadyExistException()
+        {
+        }
+
+        public ItemAlreadyExistException(string message) : base(message)
+        {
+            Console.WriteLine("item already exist");
+        }
+
+        public ItemAlreadyExistException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ItemAlreadyExistException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ ConsoleApp24/ItemNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SecretAgent
+{
+    [Serializable]
+    internal class ItemNotFoundException : Exception
+    {
+        public ItemNotFoundException()
+        {
+        }
+
+        public ItemNotFoundException(string message) : base(message)
+        {
+            Console.WriteLine("Item not found");
+        }
+
+        public ItemNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ItemNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
